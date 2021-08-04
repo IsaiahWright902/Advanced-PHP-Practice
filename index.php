5417ca36eb92da0333bff3103db001ccd7388ed7
IsaiahWright902 <icwright902@gmail.com>
2021-08-04 10:34:00 -0400

    Header and Footer

--- index.php
+++ index.php
@@ -1,26 +1,14 @@
 <?php
-// include and require: 
-include('ninjas.php');
-require('ninjas.php');
-echo 'end of php'
 
 ?>
 
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>More Advanced PHP Practice</title>
-</head>
+<?php include('templates/header.php') ?>
 
-<body>
+<?php include('templates/footer.php') ?>
 
 
-
-</body>
 
 </html>
---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+
+<footer class="section">
+    <div class="center grey-text">Copyright doesn't exist</div>
+</footer>
+</body> 
