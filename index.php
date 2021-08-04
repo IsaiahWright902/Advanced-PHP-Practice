d14f836ca0987d8533ddb5c16586db14f209c837
IsaiahWright902 <icwright902@gmail.com>
2021-08-04 16:09:53 -0400

    explode function and pizza template

--- index.php
+++ index.php
@@ -22,7 +22,8 @@ mysqli_free_result($result);
 //close connection
 mysqli_close(($conn));
 
-print_r($pizzas)
+
+explode(',', $pizzas[0]['ingredients']);
 
 
 ?>
@@ -31,6 +32,29 @@ print_r($pizzas)
 <html>
 
 <?php include('templates/header.php') ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+<div class="container">
+    <div class="row">
+        <?php foreach ($pizzas as $pizza) { ?>
+            <div class="col s6 md3 ">
+                <div class="card z-depth-0">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <ul>
+                            <?php foreach (explode(',', $pizza['ingredients']) as $ing) { ?>
+                                <li><?php echo htmlspecialchars($ing) ?></li>
+                            <?php } ?>
+                        </ul>
+                    </div>
+                    <div class="card-action right-align">
+                        <a href="#" class="brand-text">more info</a>
+                    </div>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
 
 <?php include('templates/footer.php') ?>
 
