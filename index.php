8def7c9b81954451de8594db876510141ee439fb
IsaiahWright902 <icwright902@gmail.com>
2021-08-05 09:10:09 -0400

    mysql module and post function

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-// connect to db
-$conn = mysqli_connect('localhost', 'isaiah', 'test1234', 'ninja_pizza');
-
-// check connection 
-if (!$conn) {
-    echo 'Connection error: ' . mysqli_connect_error();
-}
+include('config/db_connect.php');
 
 // write query for all pizzas
 $sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
